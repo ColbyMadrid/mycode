0f2cc3244aae68833b5b203a76b3eda5635b2dc6
ColbyMadrid <82245865+ColbyMadrid@users.noreply.github.com>
2021-06-10 17:12:39 -0700

    Add files via upload

---
+++ Birthday Card Generator/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelloWorld.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter who this card is from")]
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Please enter who the message is to")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Please enter your message")]
+        // Exercise 1 - Models
+        public string Message { get; set; }
+    }
+}
